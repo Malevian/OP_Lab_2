4e30e7602a6137c000c0ed8f64f45e1887731a5b
Malevian <89986954+Malevian@users.noreply.github.com>
2022-05-19 11:26:05 +0300

    Add files via upload

---
+++ Lab_4_cpp/Header.h
@@ -0,0 +1,33 @@
+#pragma once
+
+class Ring {
+	float cordX, cordY,
+		innerRadius, outerRadius;
+	public:
+		Ring();
+
+		Ring(float CordX, float CordY, float InnerRadius, float OuterRadius);
+
+		Ring(Ring& ring);
+
+		~Ring(){}
+
+		float thickness();
+
+		Ring operator++();
+
+		Ring operator++(int useless);
+
+		const Ring operator*=(int size);
+
+		float getcordX();
+
+		float getcordY();
+
+		float getInnerRadius();
+
+		float getOuterRadius();
+
+};
+float Find(float C1, float C2, float C3);
+void output(Ring);
